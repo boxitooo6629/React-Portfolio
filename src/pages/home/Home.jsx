368a816a4a2aa3ad46308a09a2eff2ed165bb22a
nauticus <nauticus@nauticuss-Mac-Studio.local>
2023-03-20 20:38:54 +0200

    change name

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return (
         <div className="home-page">
-            <p className="introduction"><span className="emphasis">Hi</span>, my name is <span className="emphasis">Blago.</span></p>
+            <p className="introduction"><span className="emphasis">Hi</span>, my name is <span className="emphasis">Emo.</span></p>
             <span className="avatar-container"><img className="avatar" src="./avatar.jpeg" alt="Me :)"></img></span>
         </div>
     );
